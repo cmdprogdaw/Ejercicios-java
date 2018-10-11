8fee0a60ce13163aff12cd1e362b59f1eb1042f5
Cristina <cristinamegi.cmd@gmail.com>
2018-10-11 13:16:22 +0200

    Somos los mejores (par impar)

---
+++ Ejercicios iniciación/src/Par_impar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Par_impar {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner (System.in);
+		
+		int numero;
+		System.out.println("Introduzca un número: ");
+		numero = teclado.nextInt(); 
+		
+		if (numero %2 == 0) {
+			System.out.println(numero + " es par");
+		}
+		else {
+			System.out.println(numero + " es impar");
+		}
+	}
+
+}
