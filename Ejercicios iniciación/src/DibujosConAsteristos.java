58570965a6f0d0cb2eff24c9a3a147f0c96afa8e
Cristina <cristinamegi.cmd@gmail.com>
2018-11-04 22:28:02 +0100

    *

--- Ejercicios iniciación/src/DibujosConAsteristos.java
+++ Ejercicios iniciación/src/DibujosConAsteristos.java
@@ -3,7 +3,7 @@ public class DibujosConAsteristos {
 	
 	static void volcan (int h) {
 		int espacios;
-		int  asteriscos = 2;
+		int asteriscos = 2;
 		int total = (int) Math.pow(2,h)/2;
 		for(int i = 0; i < h; i++) {
 			espacios = total - asteriscos/2; 
@@ -36,9 +36,26 @@ public class DibujosConAsteristos {
 		}
 	}
 
+	static void tablero (int n, int m) {
+		int escaque = 3;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if ((i % 2 == 0 && j % 2 != 0) || (i % 2 != 0 && j % 2 == 0)) {
+					System.out.print("*"); System.out.print("*"); System.out.print("*");
+				}
+				else {
+					System.out.print(" "); System.out.print(" ");System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 	  volcan(6);
-	  mosaico(6, 6);
+	  mosaico(8, 8);
+	  tablero (8, 8);
 	}
 
 }
